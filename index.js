952ccd6fc11367635195a2ceaf91428f8b97a075
chukajoseph99 <chukajoseph99@gmail.com>
2023-12-28 19:45:17 +0100

    Implement DOM Manipulation methods on RPS

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function getComputerChoice(){
     return choice;
 }
 
-
 // we'll use an if else statement to return who wins the round, we'll also account for ties. hope you didnt forget this &&
 function playRound(playerSelection,computerSelection){
     // console.log((playerSelection, computerSelection));
